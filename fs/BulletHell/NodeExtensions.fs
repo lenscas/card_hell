b16432ae88033d4b1daae51557331b6796918509
lenscas <lenscas@gmail.com>
2021-04-30 13:16:06 +0200

    add highscore storage

--- fs/BulletHell/NodeExtensions.fs
+++ fs/BulletHell/NodeExtensions.fs
@@ -2,6 +2,7 @@ namespace BulletHell
 
 module GDUtils =
     open Godot
+    open FSharp.Json
 
     let loadTexture name =
         GD.Load<Texture>("res://assets/" + name + ".png")
@@ -9,6 +10,62 @@ module GDUtils =
     let setTexture name (sprite: Sprite) = sprite.Texture <- loadTexture name
 
     let loadCard name = loadTexture ("cards/" + name)
+
+    type WrappedError<'a> =
+        | Godot of Error
+        | Custom of 'a
+
+    let errorToResult (err: Error) =
+        match err with
+        | Error.Ok -> Ok()
+        | x -> Result.Error(x)
+
+    let errorToWrappedResult<'a> =
+        function
+        | Error.Ok -> Ok()
+        | x -> x |> WrappedError<'a>.Godot |> Result.Error
+
+    let godotResultToWrapped<'a, 'b> (res: Result<'b, _>) =
+        match res with
+        | Ok x -> Ok x
+        | Result.Error x -> x |> WrappedError<'a>.Godot |> Result.Error
+
+    let writeFile path text =
+        let file = new File()
+
+        let res =
+            file.Open("user://" + path, File.ModeFlags.Write)
+            |> errorToResult
+            |> Result.map (fun _ -> file.StoreString text)
+
+        file.Close()
+        res
+
+    let writeObjectToFile path obj =
+        let asTxt = Json.serialize obj
+        writeFile path asTxt
+
+
+    let readFile path =
+        let file = new File()
+
+        let content =
+            file.Open("user://" + path, File.ModeFlags.Read)
+            |> errorToResult
+            |> Result.map file.GetAsText
+
+        file.Close()
+        content
+
+    let readFileAs<'t> path =
+        path
+        |> readFile
+        |> godotResultToWrapped<exn, _>
+        |> Result.bind
+            (fun x ->
+                try
+                    x |> Json.deserialize<'t> |> Ok
+                with x -> x |> Custom |> Result.Error)
 
     type Node with
         member this.getNode<'a when 'a :> Node and 'a: not struct>(path: string) =
@@ -21,3 +78,5 @@ module GDUtils =
             this.AddChild sprite
 
             sprite
+
+        member this.getChildren() = this.GetChildren() |> Seq.cast<Node>
--- fs/BulletHell/RestartFs.fs
+++ fs/BulletHell/RestartFs.fs
@@ -2,6 +2,7 @@ namespace BulletHell
 
 open Godot
 open GDUtils
+open FSharp.Json
 
 type RestartFs() as this =
     inherit Node2D()
@@ -11,6 +12,48 @@ type RestartFs() as this =
 
     member this.newScore(score: int) =
         currentScoreDisplay.Value.Text <- "Score:" + score.ToString()
+
+        for v in highScoreContainer.Value.getChildren () do
+            highScoreContainer.Value.RemoveChild(v)
+            v.QueueFree()
+
+        let mutable inserted = false
+
+        let mutable highscores =
+            match readFileAs<int list> "highscores.json" with
+            | Ok x -> x
+            | Result.Error _ -> []
+            |> Seq.collect
+                (fun x ->
+                    if (not inserted) && x < score then
+                        inserted <- true
+                        [ score; x ]
+                    else
+                        [ x ])
+            |> Seq.truncate 10
+            |> Seq.toList
+
+        if highscores.Length < 10 && not inserted then
+            highscores <- (score :: (highscores |> List.rev)) |> List.rev
+            inserted <- true
+
+
+        highscores
+        |> Seq.map string
+        |> Seq.mapi (fun x y -> x, y)
+        |> Seq.iter
+            (fun (index, highscore) ->
+                let label = new Label()
+                GD.Print(index)
+                label.SetPosition(Vector2(384F, 117F + float32 (index * 35)))
+                label.SetSize(Vector2(256F, 14F))
+                label.Text <- highscore.ToString()
+                this.AddChild(label))
+
+        if inserted then
+            writeObjectToFile "highscores.json" highscores
+            |> ignore
+
         this.Show()
 
     member this.OnRestartButtonPressed() =
